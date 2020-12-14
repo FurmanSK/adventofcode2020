6b9bd510a30028559c09aee238d47ce7b10dc889
Wes Forman <wesforman@gmail.com>
2020-12-14 10:23:31 -0500

    Finished day 2

--- day2/app.py
+++ day2/app.py
@@ -5,13 +5,16 @@ if __name__ == '__main__':
     
     # Open file
     f = open('day2/input.txt', 'r')
-    goodOnes = 0
+    goodOnesP1 = 0
+    goodOnesP2 = 0
 
+    # part 1
     for x in f:
         tmp = x.split()
         tmp[0] = tmp[0].split('-')
         tmp[1] = tmp[1].strip(':')
         
+        # Part 1
         cntr = Counter(tmp[2])
 
         min = int(tmp[0][0])
@@ -20,6 +23,11 @@ if __name__ == '__main__':
         print(tmp[1], " appears ", Counter(tmp[2])[tmp[1]], " times")
         if cntr[tmp[1]] <= max and cntr[tmp[1]] >= min:
             print("Found a good one ", tmp)
-            goodOnes += 1
+            goodOnesP1 += 1
+        
+        # Part 2
+        if bool(tmp[2][int(tmp[0][0]) - 1] == tmp[1]) ^ bool(tmp[2][int(tmp[0][1]) - 1] == tmp[1]):
+            goodOnesP2 += 1
 
-    print("Good ones = ", goodOnes)
+    print("Part 1 Good ones = ", goodOnesP1)
+    print("Part 2 Good ones = ", goodOnesP2)
