13e9b6c8834a5f883f753606d41720802a6a28a2
Wes Forman <wesforman@gmail.com>
2020-12-16 22:01:51 -0500

    Finished day 4

--- day4/day4.py
+++ day4/day4.py
@@ -1,3 +1,6 @@
+from os import truncate
+import re
+
 class Day4:
 
     def __init__(self, p):
@@ -5,16 +8,16 @@ class Day4:
 
     def solve(self):
         num = 0
-        passports = []
+        passports_p1 = []
+        passports_p2 = []
         passport = Passport()
         for x in self.puzzle:
             # Blank line
-            num += 1
-            if num == 998:
-                print("Stop here and check")
             if x == "\n":
-                if passport.check_if_valid():
-                    passports.append(passport)
+                if passport.check_if_valid_p1():
+                    passports_p1.append(passport)
+                    if passport.check_if_valid_p2():
+                        passports_p2.append(passport)
                 passport = Passport()
                 continue
             else:
@@ -23,19 +26,60 @@ class Day4:
                     attr = a.split(":")
                     setattr(passport, attr[0], attr[1])
         # Check total in passports which are the valid ones
-        return len(passports)
+        return len(passports_p1), len(passports_p2)
 
 class Passport:
     byr, iyr, eyr, hgt, hcl, ecl, pid, cid = "", "", "", "", "", "", "", ""
     
-    def check_if_valid(self):
+    def check_if_valid_p1(self):
         for x in [attr for attr in dir(self) if not callable(getattr(self, attr)) and not attr.startswith("__")]:
             if getattr(self, x) == "" and x != "cid":
                 return False
         return True
 
+    def check_if_valid_p2(self):
+        if (len(self.byr) != 4 or (int(self.byr) < 1920 or int(self.byr) > 2002)):
+            return False
+        if ( len(self.iyr) != 4 or (int(self.iyr) < 2010 or int(self.iyr) > 2020)):
+            return False
+        if (len(self.eyr) != 4 or (int(self.eyr) < 2020 or int(self.eyr) > 2030)):
+            return False
+        if not self.check_hgt():
+            return False
+        if not self.check_hcl():
+            return False
+        if not self.check_ecl():
+            return False
+        if not self.check_pid():
+            return False
 
+        # else return True
+        return True
 
+    def check_hgt(self):
+        # Determine if cm or in
+        if self.hgt.find("cm") > 0:
+            index = self.hgt.find("cm")
+            if int(self.hgt[0:index]) > 193 or int(self.hgt[0:index]) < 150:
+                return False
+            else:
+                return True
+        else:
+            index = self.hgt.find('in')
+            if int(self.hgt[0:index]) < 59 or int(self.hgt[0:index]) > 76:
+                return False
+            else:
+                return True
+
+    def check_hcl(self):
+        return re.search("^#[0-9a-f]{6}$", self.hcl)
+
+    def check_ecl(self):
+        valid_eye_color = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+        return self.ecl in valid_eye_color
+
+    def check_pid(self):
+        return re.search(r'^\d{9}$', self.pid)
 
 if __name__ == '__main__':
     
@@ -43,4 +87,5 @@ if __name__ == '__main__':
     f = open('day4/input.txt', 'r')
 
     run = Day4(f)
-    print("Part 1 = ", run.solve())
+    ret = run.solve()
+    print("Part 1 = ", ret[0], "\nPart 2 = ", ret[1])
--- day4/test_day4.py
+++ day4/test_day4.py
@@ -5,33 +5,77 @@ from day4 import Passport
 import pytest
 
 testCase  = open('day4/testdata.txt', 'r')
+testCase2  = open('day4/testdata2.txt', 'r')
 
 @pytest.fixture
 def tpuzzle():
     return Day4(testCase)
 
 @pytest.fixture
+def tpuzzle2():
+    return Day4(testCase2)
+
+@pytest.fixture
 def passport():
     pprt = Passport()
-    pprt.hcl = "#88"
+    pprt.hcl = "#885577"
     pprt.ecl = "amb"
     pprt.byr = "1930"
-    pprt.eyr = "1920"
-    pprt.pid = "555333"
+    pprt.eyr = "2021"
+    pprt.pid = "022111849"
     pprt.hgt = "182cm"
     pprt.iyr = "2013"
     pprt.cid = "244"
     return pprt
 
 def test_solve(tpuzzle):
-    assert tpuzzle.solve() == 2
+    # test part 1
+    ret = tpuzzle.solve()[1]
+    assert ret == 2
+
+def test_solve_p2(tpuzzle2):
+    # test part 1
+    ret = tpuzzle2.solve()[1]
+    assert ret == 4
 
 def test_class(tpuzzle):
     assert tpuzzle.puzzle == testCase
 
-def test_check_if_valid(passport):
-    assert passport.check_if_valid() == True
+def test_check_if_valid_p1(passport):
+    assert passport.check_if_valid_p1() == True
     passport.cid = ""
-    assert passport.check_if_valid() == True
+    assert passport.check_if_valid_p1() == True
     passport.hcl = ""
-    assert passport.check_if_valid() == False
+    assert passport.check_if_valid_p1() == False
+
+def test_check_if_valid_p2(passport):
+    assert passport.check_if_valid_p2() == True
+    passport.cid = ""
+    assert passport.check_if_valid_p2() == True
+    passport.hcl = ""
+    assert passport.check_if_valid_p2() == False
+
+def test_check_hgt(passport):
+    passport.hgt = "199cm"
+    assert passport.check_hgt() == False
+    passport.hgt = "190cm"
+    assert passport.check_hgt() == True
+    passport.hgt = "80in"
+    assert passport.check_hgt() == False
+    passport.hgt = "60in"
+    assert passport.check_hgt() == True
+
+def test_check_hcl(passport):
+    assert bool(passport.check_hcl()) == True
+    passport.hcl = "#88"
+    assert bool(passport.check_hcl()) == False
+
+def test_check_ecl(passport):
+    assert passport.check_ecl() == True
+    passport.ecl = "aaa"
+    assert passport.check_ecl() == False
+
+def test_check_pid(passport):
+    assert bool(passport.check_pid()) == True
+    passport.pid = "000"
+    assert bool(passport.check_pid()) == False
